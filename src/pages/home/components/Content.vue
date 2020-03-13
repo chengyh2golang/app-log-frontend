33089a44717bb8e6540033027b6a6850aab03cf6
chengyh2golang <chengyh2golang@gmail.com>
2020-03-14 00:26:15 +0800

    search content web finished

--- src/pages/home/components/Content.vue
+++ src/pages/home/components/Content.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="sidebar">
       <div class="search">
-        <input class="search-input" v-model="keyword" placeholder="输入日志关键字">
+          <input class="search-input" v-model="keyword" placeholder="输入日志关键字">
         <button class="btn" @click="handleQueryClick">查询</button>
       </div>
       <router-link to="/pod">
@@ -10,11 +10,24 @@
       </router-link>
 
     </div>
-    <div class="search-content">
-      <ul>
-        <li >日志内容</li>
-        <li >日志内容</li>
-      </ul>
+    <div class="search-result" v-if="show">
+      <div v-if="haveContent">
+        <div class="search-result-env" v-for="env of envArr"  :key="env">
+          <div class="search-result-env-ns" v-for="ns of fetchNsbyEnv(env)" :key="ns">
+            <div v-for="deploy of fetchDeployByNsAndEnv(env,ns)" :key="deploy">
+              <ul v-for="podName of fetchPodNameByDeployAndNsAndEnv(env,ns,deploy)"
+                  :key="podName">
+                <li>项目: {{ns}}, 应用: {{deploy}}, Pod: {{podName}}</li>
+                <li v-for="item of fetchMsgByPodNameAndDeployAndNsAndEnv(env,ns,deploy,podName)"
+                    :key="item.id">
+                  {{item.message}}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div v-else>没有查到匹配的内容！</div>
     </div>
   </div>
 
@@ -26,7 +39,15 @@
         name: "HomeContent",
         data () {
             return {
-                keyword: ""
+                keyword: "",
+                queryResultMap: Object,
+                envArr: [],
+                show: false,
+            }
+        },
+        computed: {
+            haveContent () {
+                return this.queryResultMap
             }
         },
         methods: {
@@ -34,10 +55,12 @@
                 if (!this.keyword) {
                     alert("请输入用于查询日志的关键字")
                 } else {
-                    console.log(this.keyword)
-                    // let queryUrl = '/api/querycontent?keyword="' + this.keyword + '"';
+                    if (this.queryResultMap) {
+                        this.queryResultMap = null;
+                        this.envArr = [];
+                    }
+                    this.show = true;
                     let queryUrl = '/api/querycontent?keyword=' + this.keyword;
-                    console.log(queryUrl)
                     axios.get(queryUrl).then(
                         this.getQuerySuccess
                     )
@@ -46,9 +69,64 @@
             getQuerySuccess (res) {
                 res = res.data;
                 if (res.ret && res.env_content_query_result) {
-                    console.log(res)
+                    this.queryResultMap = res.env_content_query_result;
+                    for (let envKey in this.queryResultMap) {
+                        this.envArr.push(envKey)
+                    }
                 }
 
+            },
+            fetchNsbyEnv(env) {
+                let nsList = [];
+                let queryResultArr = this.queryResultMap[env];
+                for (let i = 0,len = queryResultArr.length; i < len; i++ ){
+                    if (nsList.indexOf(queryResultArr[i]['namespace']) === -1) {
+                        nsList.push(queryResultArr[i]['namespace'])
+                    }
+                }
+                return nsList
+            },
+            fetchDeployByNsAndEnv(env,ns) {
+                let deployList = [];
+                let queryResultArr = this.queryResultMap[env];
+                for (let i = 0,len = queryResultArr.length; i < len; i++ ){
+                    if (queryResultArr[i]['namespace'] === ns) {
+                        if (deployList.indexOf(queryResultArr[i]["deploy_name"]) === -1) {
+                            deployList.push(queryResultArr[i]["deploy_name"])
+                        }
+                    }
+                }
+                return deployList
+            },
+            fetchPodNameByDeployAndNsAndEnv(env,ns,deploy) {
+                let podNameList = [];
+                let queryResultArr = this.queryResultMap[env];
+                for (let i = 0,len = queryResultArr.length; i < len; i++ ){
+                    if (queryResultArr[i]['namespace'] === ns &&
+                        queryResultArr[i]["deploy_name"] === deploy)
+                    {
+                        if (podNameList.indexOf(queryResultArr[i]["pod_name"]) === -1) {
+                            podNameList.push(queryResultArr[i]["pod_name"])
+                        }
+                    }
+                }
+                return podNameList
+            },
+            fetchMsgByPodNameAndDeployAndNsAndEnv(env,ns,deploy,podName) {
+                let MsgList = [];
+                let queryResultArr = this.queryResultMap[env];
+                for (let i = 0,len = queryResultArr.length; i < len; i++ ){
+                    let MsgObj = {};
+                    if (queryResultArr[i]['namespace'] === ns &&
+                        queryResultArr[i]["deploy_name"] === deploy &&
+                        queryResultArr[i]["pod_name"] === podName)
+                    {
+                        MsgObj["message"] = queryResultArr[i]["message"];
+                        MsgObj["id"] = queryResultArr[i]["id"];
+                        MsgList.push(MsgObj)
+                    }
+                }
+                return MsgList
             }
         }
     }
@@ -78,7 +156,8 @@
         margin-top .6rem
         border-radius .2rem
         padding .05rem
-    .search-content
+        background #25a4bb
+    .search-result
       border 2px solid #eee
       float left
       width 60%
